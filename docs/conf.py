188d41f7ee4f9d5a200e3b1d95ec4546d57b9a4a
Malte Ollenschläger <56151847+MalteOlle@users.noreply.github.com>
2021-09-25 09:32:42 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,8 @@ html_sidebars = {
    'README': []
 }
 
+sphinxemoji_style = 'twemoji'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
