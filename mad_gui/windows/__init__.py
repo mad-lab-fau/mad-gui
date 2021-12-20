29e63f3979e95bf6c1d1c23b5d42755801d1a8fb
Malte Ollenschläger <56151847+MalteOlle@users.noreply.github.com>
2021-12-20 21:31:42 +0100

    Update __init__.py

--- mad_gui/windows/__init__.py
+++ mad_gui/windows/__init__.py
@@ -2,6 +2,11 @@
 """The :py:mod:`mad_gui.windows` keeps files for creating a set of windows.
 """
 
+# from mad_gui.windows.plugin_selection import LoadDataWindow, ExportResultsWindow, BasePluginSelector
+from mad_gui.windows.video_window import VideoWindow
+from mad_gui.components.dialogs.user_information import UserInformation
+from mad_gui.windows.main import MainWindow
+
 from pathlib import Path
 
 import sys
@@ -9,11 +14,6 @@ import sys
 # make sure window_buttons_rc is found
 sys.path.append(str((Path(__file__).parent.parent.parent / "mad_gui" / "qt_designer").absolute()))
 
-
-# from mad_gui.windows.plugin_selection import LoadDataWindow, ExportResultsWindow, BasePluginSelector
-from mad_gui.windows.video_window import VideoWindow
-from mad_gui.components.dialogs.user_information import UserInformation
-from mad_gui.windows.main import MainWindow
 
 __all__ = [
     "MainWindow",
