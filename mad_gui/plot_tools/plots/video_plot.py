4824f8d6caa9fba02eadfef6ce73d0b418db9ad6
Malte Ollenschlaeger <malte.ollenschlaeger@fau.de>
2022-01-31 17:07:25 +0100

    Clean video plot

--- mad_gui/plot_tools/plots/video_plot.py
+++ mad_gui/plot_tools/plots/video_plot.py
@@ -2,7 +2,6 @@
 class."""
 
 import numpy as np
-import pandas as pd
 
 from mad_gui.models.local import PlotData
 from mad_gui.models.ui_state import MODES
@@ -27,9 +26,6 @@ class VideoPlot(BasePlot):
     }
 
     def __init__(self, parent=None, video_window=None):
-        # plot_data = PlotData(data=pd.DataFrame(data=[], columns=["time"]), sampling_rate_hz=1)
-        # plot_data.annotations = {}
-
         super().__init__(label_classes=[BaseRegionLabel], parent=parent)
         StateKeeper.video_duration_available.connect(self.update_video_duration)
         self.state = SensorPlotState()
